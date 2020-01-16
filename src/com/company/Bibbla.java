0f3c2ad3445e9026ef6998de0fb2869bf2d671fe
Sebbe Lundgren <moment928@gmail.com>
2020-01-16 22:42:49 +0100

    Added menu for staff

--- src/com/company/Bibbla.java
+++ src/com/company/Bibbla.java
@@ -48,4 +48,58 @@ public class Bibbla {
         exitBibbla = true;
     }
 
+    private void logIn {
+        System.out.println("Ange användarnamn: ");
+        Scanner scanner = new Scanner(System.in);
+        String userName = scanner.nextLine();
+
+        if (userName == "staff"){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Välkommen bibliotekarie till Smistorps bibblas portal. Vad vill du göra?");
+            int choice;
+
+            while (true) {
+                System.out.println("1. Söka efter användare " + "\n" +
+                        "2. Lägga till bok " + "\n" +
+                        "3. Ta bort bok " + "\n" +
+                        "4. Se tillgängliga böcker " + "\n" +
+                        "5. Se utlånade böcker " + "\n");
+
+                try {
+                    choice = Integer.parseInt(scanner.nextLine());
+                } catch (Exception e) {
+                    System.out.println("Enbart nummer!");
+                    continue;
+                }
+
+                if (choice > 0 && choice <= 5) {
+                    break;
+                } else {
+                    System.out.println("Enbart 1 eller 2, försök igen!");
+                }
+
+            }
+
+            switch (choice) {
+
+                case 1:
+                    searchForUser();
+                    break;
+                case 2:
+                    addBook();
+                    break;
+                case 3:
+                    deleteBook();
+                    break;
+                case 4:
+                    seeAvailableBooks();
+                    break;
+                case 5:
+                    seeRentedBooks();
+                    break;
+            }
+        }
+        }
+    }
+
 }
