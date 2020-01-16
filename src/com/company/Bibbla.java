03c6779f081e5caaad5527493160a737b68457d9
Sebbe Lundgren <moment928@gmail.com>
2020-01-16 22:20:46 +0100

    Added a main menu

---
+++ src/com/company/Bibbla.java
@@ -0,0 +1,43 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Bibbla {
+
+    private void mainMenu() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("---- Welcome to Smistorps Bibbla ----");
+        int choice;
+
+        while (true) {
+            System.out.println("1. Logga in " + "\n" +
+                    "2. Avbryt " + "\n" +
+                    "3. Exit game " + "\n");
+
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (Exception e) {
+                System.out.println("Enbart nummer!");
+                continue;
+            }
+
+            if (choice > 0 && choice <= 2) {
+                break;
+            } else {
+                System.out.println("Enbart 1 eller 2, försök igen!");
+            }
+
+        }
+
+        switch (choice) {
+
+            case 1:
+                logIn();
+                break;
+            case 2:
+                exitBibbla();
+                break;
+        }
+    }
+
+}
