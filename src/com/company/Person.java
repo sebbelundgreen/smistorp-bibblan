11115e1760f0698878bf969d8e77b4ebc5916537
Sebbe Lundgren <moment928@gmail.com>
2020-01-16 23:21:26 +0100

    Added membersnumber and name to Person

--- src/com/company/Person.java
+++ src/com/company/Person.java
@@ -1,4 +1,14 @@
 package com.company;
 
+import javax.xml.namespace.QName;
+
 public class Person {
+    private String name;
+    private String membernum;
+
+
+    public Person(String name, String membernum) {
+        this.name = name;
+        this.membernum = membernum;
+    }
 }
